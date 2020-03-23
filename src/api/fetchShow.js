853c3641734010350dc4a969e2a19bab71f9481f
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-23 15:49:12 -0400

    adds tests to episodes component to ensure it renders empty and rerenders with a full season

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -7,9 +7,4 @@ export const fetchShow = () => {
           "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
         )
     )
-    //   .then(res => {
-    //     // pay attention to how this is setting the state
-    //     setShow(res.data);
-    //     setSeasons(formatSeasons(res.data._embedded.episodes));
-    //   });
   };
